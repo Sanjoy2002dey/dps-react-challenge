272ece8ef9de42f7a6bc6d13a11e7c5c99e04327
Sanjoy Dey <113011480+Sanjoy2002dey@users.noreply.github.com>
2024-07-31 13:32:36 +0530

    Fetched user data from dummy API.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import dpsLogo from './assets/DPS.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import dpsLogo from './assets/DPS.svg';
+
 
 function App() {
+	const [users, setUsers] = useState([]);
+	const [searchTerm, setSearchTerm] = useState('');
+	const [selectedCity, setSelectedCity] = useState('');
+	const [highlightOldest, setHighlightOldest] = useState(false);
+
+	useEffect(() => {
+		fetch('https://dummyjson.com/users')
+			.then((response) => response.json())
+			.then((data) => setUsers(data.users));
+	}, []);
+	console.log(users);
 	return (
 		<>
 			<div>
 				<a href="https://www.digitalproductschool.io/" target="_blank">
 					<img src={dpsLogo} className="logo" alt="DPS logo" />
 				</a>
+
 			</div>
-			<div className="home-card">
-				<p>Your solution goes here 😊</p>
-			</div>
+
 		</>
 	);
 }
